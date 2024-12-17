3eaa4edd820fe1f5bca4db261c07c2b1f066aa62
Eloi RM <eloimailmaynad@gmail.com>
2024-12-17 14:03:56 +0100

    added : day and night filter

--- scenes/day_night_cycle.gd
+++ scenes/day_night_cycle.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+@onready var night_filter: CanvasModulate = $"../../NightFilter"
+
 var night : bool = false :
 	set(value) :
 		night = value
 		if night == true :
 			%DayOrNight.text = "Night"
+			night_filter.color = Color(0.2, 0.3, 0.6)
 		else :
 			%DayOrNight.text = "Day"
+			night_filter.color = Color(1, 1, 1)
 
 var second : int :
 	set(value) :
