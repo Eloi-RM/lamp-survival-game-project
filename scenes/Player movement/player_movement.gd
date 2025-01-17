4db403c876a907040bc57c646d08f138f4e47899
Raika <pejuvenelle@hotmail.com>
2025-01-17 13:12:08 +0100

    ajout fonction de déplacement

---
+++ scenes/Player movement/player_movement.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@export var speed = 300
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move-up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move-down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move-right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move-left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed;
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk-left"
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+		
+	if velocity.y > 0:
+		$AnimatedSprite2D.animation = "walk-down"
+		
+	if velocity.y < 0:
+		$AnimatedSprite2D.animation = "walk-up"
